476be2202016e69e95cd61b36a5f501278b28355
transformGit <2630243397@qq.com>
2016-11-05 23:32:37 +0800

    使用connect模块

--- server.js
+++ server.js
@@ -1,37 +1,61 @@
 var http = require('http');
 var parseUrl = require('url').parse;
 var data = require('./models/data');
+var connect = require('connect');
+var app = connect();
 
-http.createServer(function(req, res){
-	function send(html){
+function getNews(id){
+	return data[id] || '文章不存在!'
+}
+app.use(function(req,res,next){
+	res.send = function send(html){
 		res.writeHead(200, {
 			'Content-Type': 'text/html;charset=utf-8'
 		})
 		res.end(html)
 	}
-	function getNews(id){
-		return data[id] || '文章不存在!'
-	}
+	next()
+})
+app.use(function(req,res,next){
 	var info = parseUrl(req.url, true);
-	console.log(info)
-	if(info.pathname == '/'){
-		send(
-			'<ul>'+
-				'<li><a href="/item?cat=1&id=1">第一篇</a></li>'+
-				'<li><a href="/item?cat=1&id=2">第二篇</a></li>'+
-				'<li><a href="/item?cat=1&id=3">第三篇</a></li>'+
-				'<li><a href="/item?cat=1&id=4">第四篇</a></li>'+
-				'<li><a href="/item?cat=1&id=5">第五篇</a></li>'+
-				'<li><a href="/item?cat=1&id=6">第六篇</a></li>'+
-			'</ul>'
-		)
-  }else if(info.pathname == '/item' && info.query.cat == '1'){
-		send(getNews(info.query.id))
+	req.pathname = info.pathname;
+	req.query = info.query;
+	next()
+})
+app.use(function(req,res,next){
+		if(req.pathname == '/'){
+			res.send(
+				'<ul>'+
+					'<li><a href="/item?cat=1&id=1">第一篇</a></li>'+
+					'<li><a href="/item?cat=1&id=2">第二篇</a></li>'+
+					'<li><a href="/item?cat=1&id=3">第三篇</a></li>'+
+					'<li><a href="/item?cat=1&id=4">第四篇</a></li>'+
+					'<li><a href="/item?cat=1&id=5">第五篇</a></li>'+
+					'<li><a href="/item?cat=1&id=6">第六篇</a></li>'+
+					'<li><a href="/help">帮助</a></li>'+
+				'</ul>'
+			)
+  	}else{
+			next()
+  	}
+})
+app.use(function(req,res,next){
+	if(req.pathname == '/item' && req.query.cat == '1'){
+		res.send(getNews(req.query.id))
 	}else{
-		send('<h1>页面找不到！</h1>')
+		next()
 	}
-	// console.log(req.method)
-	// console.log(req.url)
-	// console.log(req.headers)
-}).listen(3000)
+})
+app.use(function(req,res,next){
+	if(req.pathname == '/help'){
+		res.send('这里是帮助。')
+	}else{
+		next()
+	}
+})
+app.use(function(req,res,next){
+	res.send('<h1>页面找不到！</h1>')
+})
+var server = http.createServer(app)
+server.listen(3000)
 console.log('server running at post 3000')
